44309b3e145b6a7a1941df101ba2ce004a7f649c
stanislavmircic <stanislavmircic@gmail.com>
2019-12-20 10:41:47 +0100

    Frequency of stimulation is 10Hz and pulse width 80uSec

--- V0_953/Human-to-Human-SpikerBox/Human-to-Human-SpikerBox.ino
+++ V0_953/Human-to-Human-SpikerBox/Human-to-Human-SpikerBox.ino
@@ -1,6 +1,6 @@
 /*
     ----------------------------------------------------------------------------------------------------
-    Backyard Brains 24. Oct. 2018
+    Backyard Brains 20. Dec. 2019
 
     Made for HHI SpikerBox V1(0.953)
     Based on Arduino UNO ATMEGA 328
@@ -8,8 +8,8 @@
     TENS device with communication with Spike Recorder
 
     Parameters of stimulation are fixed:
-    Pulse width: 100 micro seconds
-    Frequency:   120Hz
+    Pulse width: 80 micro seconds
+    Frequency:   10Hz
 
     Code sends RAW emg to Spike Recorder via serial interface. It is calculating envelope based on RAW EMG
     that is expected to float on Vcc/2. Based on envelope it updates VU meter and creates stimulation square
@@ -42,23 +42,29 @@
     D13 - VU LED 6
 
 
-    V0.5
+    V0.6
     History:
     V0.2 Glitches in communication fixed in V0.2 amd sampling frequency lower.
     V0.3 Serial communication implemented with Serial.write and batery voltage is measured every 3 sec
     V0.4 Block of TENS after long activation
     V0.5 Block TENS from starting if potentiometer is not at minimum
+    V0.6 Main timer now works 10 times the sampling freq. so that we can have greater resolution for width of stimulation pulse
     Written by Stanislav Mircic
 
     ----------------------------------------------------------------------------------------------------
 */
+
+#define FREQUENCY_OF_STIMULATION 10.0 //In Hz
+#define TENS_OF_MICROSECONDS_LENGTH_OF_STIMULATION 8 //80uSec (can not be greater than 10)
+float percentageOfStimulationInSixSeconds = 50.0;
+
 #include <avr/sleep.h>//this AVR library contains the methods that controls the sleep modes
 float lowPowerVoltage = 8.1;//In volts
 float shutDownVoltage = 7.1;//In volts
 //if this % of six second period stimulation is over threshold we will disable TENS and
 //wait for TENS to be at least 3 without stimullation over threshold and than enable TENS again
-float percentageOfStimulationInSixSeconds = 50.0;
-#define FREQUENCY_OF_STIMULATION 120.0 //In Hz
+
+
 
 #define POWER_STATE_GOOD 0
 #define POWER_STATE_LOW 1
@@ -79,9 +85,13 @@ float percentageOfStimulationInSixSeconds = 50.0;
 #define SENSITIVITY_BUTTON_PIN 3                //pin for button that changes sensitivity
 #define MAX_NUMBER_OF_CHANNELS 2                //maximum number of analog channels
 
-int interrupt_Number = 198;//198;                     // Output Compare Registers  value = (16*10^6) / (Fs*8) - 1
+int interrupt_Number = 19;//198;                     // Output Compare Registers  value = (16*10^6) / (Fs*8) - 1
 // Set to 198 for 10000 Hz max sampling rate
 // Used for main timer that defines period of measurements
+
+volatile uint8_t counterOfPeriods= 0;
+volatile uint16_t emgSample = 0;
+volatile uint16_t baterySample = 0;
 
 #define MESSAGE_BUFFER_SIZE 100                 //size of buffer used to send messages to serial (to PC)
 //it needs to be big enough to hold start escape sequence,
@@ -95,14 +105,14 @@ byte endOfescapeSequence[ESCAPE_SEQUENCE_LENGTH] = {255, 255, 1, 1, 129, 255};  
 byte messageSending = 0;                        //flag that signals TX handler to send data from "messageBuffer"
 byte lengthOfMessasge = 0;                      //length of message in message buffer
 
-volatile uint16_t samplingBuffer[MAX_NUMBER_OF_CHANNELS];     //main buffer that contains real measurements
+
 
 
 int incrementsForLEDThr[] = {28, 53, 81, 108, 135, 161};      //threshold intervals for different sensitivities (sens-30)/6
 //for example:for value = 28. EMG envelope needs to increase 28
 //AD units to light up one more LED
 
-int lastSensitivitiesIndex = 2;                               //index of sensitivity from sensitivities[] that is currently in use
+int8_t lastSensitivitiesIndex = 2;                               //index of sensitivity from sensitivities[] that is currently in use
 byte sensitivityButtonPressed = 0;                            //used to ignore glitches from button bounce
 uint16_t sensitivityVisualFeedbackCounter = 0;                //timer counter that holds selected sensitivity LED ON when user changes
 //sensitivity with button press
@@ -137,6 +147,7 @@ uint16_t counterForPeriodOfStimulation = 0;
 
 uint16_t shutDownVoltageInADUnits = 0;
 uint16_t lowPowerVoltageInADUnits = 0;
+uint16_t lowPowerVoltageInADUnitsPlus20 = 0;
 
 byte powerState = POWER_STATE_GOOD;
 uint16_t powerInADUnits = 1023;
@@ -174,6 +185,12 @@ void setup()
 
   digitalWrite(9, LOW); //do not power collector of driving transistor
   digitalWrite(5,HIGH); //turn on the transistor
+
+  //debug pins ---------------------------------------------
+   pinMode(16, OUTPUT);//a2
+   pinMode(17, OUTPUT);//a3
+   pinMode(18, OUTPUT);//a4
+  //debug pins---------------------------------------------
   
 
    //test potentiometer position
@@ -212,6 +229,7 @@ void setup()
 
 
   lowPowerVoltageInADUnits = (uint16_t)((lowPowerVoltage / 15.0) * 1023);
+  lowPowerVoltageInADUnitsPlus20 = lowPowerVoltageInADUnits+20;
   shutDownVoltageInADUnits = (uint16_t)((shutDownVoltage / 15.0) * 1023);
 
   if (percentageOfStimulationInSixSeconds > 100)
@@ -223,7 +241,7 @@ void setup()
 
   powerState = POWER_STATE_GOOD;
   //put initial measurement of voltage to highest to avoid going in sleep mode
-  samplingBuffer[1] = 1023;
+  baterySample = 1023;
   //turn ON Green LED
   PORTD &= B11101111;
   PORTD |= B01000000;
@@ -291,9 +309,9 @@ void loop()
   if (readyToDoAuxComputation == 1)
   {
     readyToDoAuxComputation = 0;
-
+/**/
     //----------------------- POWER MANAGEMENT --------------------------
-    powerInADUnits =  samplingBuffer[1];// = 1023;
+    powerInADUnits =  baterySample;// = 1023;
 
     if (powerState == POWER_STATE_GOOD)
     {
@@ -320,7 +338,7 @@ void loop()
 
 
       }
-      else if (powerInADUnits > lowPowerVoltageInADUnits + 20)
+      else if (powerInADUnits > lowPowerVoltageInADUnitsPlus20)
       {
         powerState = POWER_STATE_GOOD;
         //green
@@ -338,7 +356,7 @@ void loop()
     {
 
       //--------------- CALCULATE ENVELOPE ------------------------
-      tempEnvValue = samplingBuffer[0];
+      tempEnvValue = emgSample;
       if (tempEnvValue < NOISE_FLOOR_FOR_ENVELOPE)
       {
         tempEnvValue = 0;
@@ -451,7 +469,7 @@ void loop()
       //---------------------------------- SENSITIVITY BUTTON -----------------------------------------------
 
       //check if button is pressed (HIGH)
-      if (digitalRead(SENSITIVITY_BUTTON_PIN))
+      if (PIND & B00001000)//pin D3
       {
         if (sensitivityButtonPressed == 0) //if button was not pressed when we scan last time
         {
@@ -540,7 +558,12 @@ void loop()
       }
 
     }
-  }//end of aux. computation
+
+
+//debug
+   
+
+    }//end of aux. computation
 
 
 
@@ -548,6 +571,9 @@ void loop()
   //----------------------------------SENDING DATA ----------------------------------------------------------
   if (outputBufferReady == 1  ) //if we have new data
   {
+   
+  
+    PORTC |= B00000100;   //debug
 
     outputBufferReady = 0;//this will be zero until we send whole frame buffer and fill it again
     //since we want to do aux computation (LEDs, relay, servo) only once per sample period
@@ -557,6 +583,7 @@ void loop()
 
     //Sends first byte of frame. The rest is sent by TX handler.
     Serial.write(outputFrameBuffer, 2);
+    PORTC &= B11101011;//debug
   }//end of detection of fresh frame data
 
 
@@ -566,7 +593,6 @@ void loop()
     messageSending = 0;
     Serial.write(messageBuffer, lengthOfMessasge - 1);
   }
-
 
 }//end of main loop
 //---------------------------------------------- END OF MAIN LOOP ---------------------------------------------------------
@@ -585,45 +611,57 @@ void serialEvent()
 
 //------------------------------------------ SAMPLING TIMER INTERRUPT --------------------------------------------------
 ISR(TIMER1_COMPA_vect) {
+PORTC |= B00001000; //debug
+  counterOfPeriods++;
+  if(counterOfPeriods == TENS_OF_MICROSECONDS_LENGTH_OF_STIMULATION)
+  {
+      PORTB &= B11111101;//PB1 (D9) high freq stim  
+  }
+  if(counterOfPeriods==10)
+  { 
+            counterOfPeriods=0;
+            if (measurementTimerForBatery == 0)
+            {
+              ADMUX =  B01000101;                                          //Start ADC Conversions
+            }
+            else
+            {
+              ADMUX =  B01000000;                                           //Start ADC Conversions
+            }
+            ADCSRA |= B01000000;                                          //do this at the begining since ADC
+                                              //signal main loop to send frame
+          
+            if (emgCrossedTheThreshold && stimulationEnabled)
+            {
+              counterForPeriodOfStimulation--;
 
-  if (measurementTimerForBatery == 0)
-  {
-    ADMUX =  B01000101;                                          //Start ADC Conversions
-  }
-  else
-  {
-    ADMUX =  B01000000;                                           //Start ADC Conversions
-  }
-  ADCSRA |= B01000000;                                          //do this at the begining since ADC
-  //can work in paralel with other stuff
+              if (counterForPeriodOfStimulation == 0)
+              {
+                PORTB |= B00000010;//PB1 (D9) high freq stim
+                counterForPeriodOfStimulation = periodOfStimulationExpressedInPeriodsOfSampling;
+              }
+              else
+              {
+                PORTD &= B01111111;//PD7 (D7) LED yellow
+              }
+              PORTD |= B10000000;//PD7 (D7) LED yellow
+              PORTD |= B00100000;//PD5 (D5) enable stim
+            }
+            else
+            {
+              counterForPeriodOfStimulation = periodOfStimulationExpressedInPeriodsOfSampling;
+              PORTD &= B01011111;//PD7 (D7) and PD5 (D5) (LED yellow and enable stim)
+              PORTB &= B11111101;//PB1 (D9) high freq stim
+            }
 
-  outputFrameBuffer[0] =  (samplingBuffer[0] >> 7) | 0x80;       //convert data to frame according to protocol
-  outputFrameBuffer[1] =  samplingBuffer[0] & 0x7F;             //first bit of every byte is used to flag start of the framr
-  outputBufferReady = 1;                                        //signal main loop to send frame
-
-  if (emgCrossedTheThreshold && stimulationEnabled)
-  {
-    counterForPeriodOfStimulation--;
-    digitalWrite(7, HIGH);
-    if (counterForPeriodOfStimulation == 0)
-    {
-      counterForPeriodOfStimulation = periodOfStimulationExpressedInPeriodsOfSampling;
-      PORTB |= B00000010;//PB1
-    }
-    else
-    {
-      digitalWrite(7, LOW);
-      PORTB &= B11111101;//PB1
-    }
-    PORTD |= B00100000;
+            //can work in paralel with other stuff
+          
+            outputFrameBuffer[0] =  (emgSample >> 7) | 0x80;       //convert data to frame according to protocol
+            outputFrameBuffer[1] =  emgSample & 0x7F;             //first bit of every byte is used to flag start of the framr
+            outputBufferReady = 1;      
   }
-  else
-  {
-    counterForPeriodOfStimulation = periodOfStimulationExpressedInPeriodsOfSampling;
-    digitalWrite(7, LOW);
-    PORTD &= B11011111;// PD5
-    PORTB &= B11111101;//PB1
-  }
+  
+  PORTC &= B11110111; //debug
 }
 
 
@@ -634,11 +672,11 @@ ISR(ADC_vect)
 {
   if (measurementTimerForBatery == 0)
   {
-    samplingBuffer[1] = ADCL | (ADCH << 8);      // store lower and higher byte of ADC
+    baterySample = ADCL | (ADCH << 8);      // store lower and higher byte of ADC
   }
   else
   {
-    samplingBuffer[0] = ADCL | (ADCH << 8);      // store lower and higher byte of ADC
+    emgSample = ADCL | (ADCH << 8);      // store lower and higher byte of ADC
   }
   measurementTimerForBatery++;
 }
